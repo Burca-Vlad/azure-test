830c467e84df774a58650a9bd82e3cfeac659646
Vlad Burca <barky1ro@gmail.com>
2022-02-15 13:58:56 +0100

    bla
    Initial commit

---
+++ vlad/src/main/java/com/example/azure/vlad/VladApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure.vlad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VladApplication.class, args);
+	}
+
+}
---
+++ vlad/src/test/java/com/example/azure/vlad/VladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure.vlad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
